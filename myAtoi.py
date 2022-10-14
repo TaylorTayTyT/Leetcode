214ffdcb1af188354faa9a6961a503ec4d3fb1e6
TaylorTayTyT <taylornguyen904@gmail.com>
2022-10-14 14:00:00 -0400

    kjj

--- myAtoi.py
+++ myAtoi.py
@@ -9,30 +9,35 @@ class Solution(object):
         """
         pos = True
         onlyNumString = ""
-        space = 0
+        s = s.strip()
         for i in range(len(s)):
-            if(ord(s[i]) == 32 and space == 0):
+            if (i == 0 and (ord(s[i]) == 43 or ord(s[i]) == 45)):
+                if ord(s[i]) == 45:
+                    pos = False
                 continue
-            if (space == 0 and ((ord(s[i]) == 43) or ord(s[i]) == 45)):
+                
+            elif (ord(s[i]) <= 57 and ord(s[i]) >= 48):
                 onlyNumString += s[i]
-            elif(ord(s[i]) > 57 and ord(s[i]) < 48):
-                break
-            onlyNumString += s[i]
-            space += 1
-            
-        int_S = int(onlyNumString)
-        
-        if(int_S < 0):
-            int_S *= 1
-            pos = False
-        bitLength = int_S.bit_length()
-        if (bitLength <= 32 and (not pos)) or (bitLength <= 31 and (pos)):
-            return int(onlyNumString)
-        
-        if(pos):
-            return pow(2, 32) - 1
-        else:
-            return pow(2,32) * -1
+                continue
+            break
 
-    print(myAtoi("12a"))
-        
+        if(onlyNumString == ""):
+            return 0
+        if(len(onlyNumString) == 1 and (ord(onlyNumString[0]) == 43 or ord(onlyNumString[0]) == 45)):
+            return 0
+        int_S = int(onlyNumString)
+
+        bitLength = int_S.bit_length()
+        if (int_S >> 31 == 0 and (not pos)) or (int_S >> 31 == 0 and (pos)):
+            if pos:
+                return int(onlyNumString)
+            return int(onlyNumString) * -1
+            
+
+        if (pos):
+            return pow(2, 31) - 1 * pos
+        else:
+            return pow(2, 31) * -1
+
+    print(myAtoi("2147483648"))
+    print(2147483646 >> 30)
