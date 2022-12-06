17d779853fc46c29634d89f3bdca0a7e2e769200
TaylorTayTyT <taylornguyen904@gmail.com>
2022-12-06 14:20:35 -0500

    maxsquarelength [simple]

--- maxSquare.java
+++ maxSquare.java
@@ -1,5 +1,7 @@
+import java.math.*;
 class maxSquare {
     public static void maxSquare(char[][] matrix) {
         System.out.println("hi");
+        int maxSquareLength = Math.min(matrix.length, matrix[0].length);
     }
 }
